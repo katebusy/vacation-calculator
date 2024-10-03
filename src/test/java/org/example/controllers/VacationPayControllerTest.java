99546ae538454f2c81c9be2f347a133431997ce5
Ekaterina Busygina <0105kate54@gmail.com>
2024-10-03 13:17:22 +0300

    changed VacationPayControllerTest tests

--- src/test/java/org/example/controllers/VacationPayControllerTest.java
+++ src/test/java/org/example/controllers/VacationPayControllerTest.java
@@ -1,64 +1,76 @@
 package org.example.controllers;
 
-import org.example.calculators.VacationPayService;
+import jdk.jfr.ContentType;
+import org.example.services.VacationPayService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitConfig({
         VacationPayController.class
 })
+@MockBean(VacationPayService.class)
+@WebMvcTest
 class VacationPayControllerTest {
 
     @Autowired
-    VacationPayController vacationPayController;
+    private VacationPayController vacationPayController;
 
-    @MockBean
+    @Autowired
     private VacationPayService vacationPayService;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void calculateTwoArgs() {
-        double yearSalary = 5000000;
-        int vacationDays = 10;
+    void calculateTwoArgs() throws Exception {
         double expected = 142207.053;
         when(vacationPayService.calculateVacationSalary(anyDouble(), anyInt())).thenReturn(expected);
 
-        ResponseEntity<Double> response = vacationPayController.calculate(yearSalary, vacationDays);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody()).isGreaterThan(0);
-
-        assertThat(response.getBody()).isEqualTo(expected);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/calculate")
+                        .param("salary", "5000000")
+                        .param("vacationDays", "10")
+                ).andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("$").value(expected));
 
         verify(vacationPayService).calculateVacationSalary(anyDouble(), anyInt());
         verifyNoMoreInteractions(vacationPayService);
     }
 
     @Test
-    void calculateThreeArgs() {
-        double yearSalary = 5000000;
-        LocalDate vacationBegin = LocalDate.parse("2024-04-29");
-        LocalDate vacationEnd = LocalDate.parse("2024-05-10");
+    void calculateThreeArgs() throws Exception {
         double expected = 142207.053;
         when(vacationPayService.calculateVacationSalary(anyDouble(), any(), any())).thenReturn(expected);
 
-        ResponseEntity<Double> response = vacationPayController.calculate(yearSalary, vacationBegin, vacationEnd);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/calculate")
+                        .param("salary", "5000000")
+                        .param("vacationBegin", "2024-04-29")
+                        .param("vacationEnd", "2024-05-10")
+                ).andExpectAll(
+                        MockMvcResultMatchers.status().isOk(),
+                        MockMvcResultMatchers.jsonPath("$").value(expected));
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isNotNull();
-        assertThat(response.getBody()).isGreaterThan(0);
-
-        assertThat(response.getBody()).isEqualTo(expected);
 
         verify(vacationPayService).calculateVacationSalary(anyDouble(), any(), any());
         verifyNoMoreInteractions(vacationPayService);
