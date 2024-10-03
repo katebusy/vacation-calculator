624b5428eb1a49a29bc9f2bfc169ab5695314d36
Ekaterina Busygina <0105kate54@gmail.com>
2024-10-03 13:18:45 +0300

    changed VacationPayControllerTest imports

--- src/test/java/org/example/controllers/VacationPayControllerTest.java
+++ src/test/java/org/example/controllers/VacationPayControllerTest.java
@@ -1,28 +1,17 @@
 package org.example.controllers;
 
-import jdk.jfr.ContentType;
+
 import org.example.services.VacationPayService;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.time.LocalDate;
-import java.util.regex.Matcher;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitConfig({
         VacationPayController.class
